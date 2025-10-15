e3ec3265ef70bb9832f05680c5d04c68a97ebc11
johnikamai <johnikamai2006@gmail.com>
2025-10-15 19:27:02 +0530

    Update and rename dif_inheritance to single_inheritance.cpp

---
+++ single_inheritance.cpp
@@ -0,0 +1,24 @@
+//single inheritance
+
+#include<iostream>
+using namespace std;
+class vehicle{
+public:
+vehicle(){
+cout<<"This is a vehicle"<<endl;}
+};
+class car:public vehicle
+{
+public:
+car(){
+cout<<"This vehicle is a car"<<endl;
+}
+};
+int main()
+{
+car obj;
+return 0;
+}
+
+
+
